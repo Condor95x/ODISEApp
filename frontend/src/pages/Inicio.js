3feea24c774a8b4ad74a814a8bf961329dfded15
Condor95x <andres.sanchez0508@gmail.com>
2025-09-10 15:50:23 +0200

    Update Inicio.js - imgs mejoradas

--- frontend/src/pages/Inicio.js
+++ frontend/src/pages/Inicio.js
@@ -8,11 +8,11 @@ function Inicio() {
     <div className="max-w-screen-lg mx-auto px-4">
      <div className="titulo-seccion">
         <h1>Bienvenido</h1>
-        <img src={odiseaLogo} alt="Logo de Odisea" className="logo-app" />
+        <img src={odiseaLogo} alt="Logo de Odisea" className="w-32 h-auto" />
       </div>
-      <div className="flex flex-row items-center justify-center gap-12 p-6 bg-white rounded-xl shadow-lg max-w-2xl w-full">
-          <img src={Meteo} alt="Meteo" className="logo-app" />
-          <img src={Resumen} alt="Resumen" className="logo-app" />
+      <div className="flex flex-wrap items-center justify-center gap-6 p-6 bg-white rounded-xl shadow-lg max-w-2xl w-full">
+          <img src={Meteo} alt="Meteo" className="w-32 h-auto" />
+          <img src={Resumen} alt="Resumen" className="w-32 h-auto" />
       </div>
     </div>
   );
