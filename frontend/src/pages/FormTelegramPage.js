3f35321beb18e092772de5082e083feacbe72944
Condor95x <andres.sanchez0508@gmail.com>
2025-09-24 17:33:53 +0200

    Create FormTelegramPage.js - TELEGRAM 4

---
+++ frontend/src/pages/FormTelegramPage.js
@@ -0,0 +1,24 @@
+// src/pages/FormTelegramPage.js
+import React, { useState } from "react";
+import FormTelegram from "../components/FormTelegram";
+
+export default function FormTelegramPage() {
+  const [newOperacion, setNewOperacion] = useState({
+    tipo_operacion: "",
+    responsable_id: "",
+    fecha_inicio: "",
+    nota: "",
+    parcela_id: "",
+    estado: "",
+    fecha_fin: "",
+    comentario: "",
+    inputs: []
+  });
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Crear Operación</h2>
+      <FormTelegram newOperacion={newOperacion} setNewOperacion={setNewOperacion} />
+    </div>
+  );
+}
